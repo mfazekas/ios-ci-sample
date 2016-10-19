7a2338c97ec6d508d217e2bac874fd26d61a4539
Miklos Fazekas <mfazekas@szemafor.com>
2016-10-19 22:07:21 +0200

    Intitial revision

--- ios-ci-sample/ViewController.swift
+++ ios-ci-sample/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var label: UILabel!
+
+    @IBAction func sayHello(_ sender: AnyObject) {
+        label.text = "Said hello!"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- ios-ci-sampleUITests/ios_ci_sampleUITests.swift
+++ ios-ci-sampleUITests/ios_ci_sampleUITests.swift
@@ -31,6 +31,8 @@ class ios_ci_sampleUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let app = XCUIApplication()
+        app.buttons["Say hello"].tap()
     }
     
 }
