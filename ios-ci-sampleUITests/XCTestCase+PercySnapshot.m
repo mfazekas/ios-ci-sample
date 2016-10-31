3dd7a1bd27f9bccdd1d5a32ff33b71ffb3cf76bc
Miklos Fazekas <mfazekas@szemafor.com>
2016-10-31 23:09:16 +0100

    Percy-ios refactor, cleanup

---
+++ ios-ci-sampleUITests/XCTestCase+PercySnapshot.m
@@ -0,0 +1,29 @@
+//
+//  XCUITestCase+PercySnapshot.m
+//  ios-ci-sample
+//
+//  Created by Miklos Fazekas on 30/10/16.
+//  Copyright © 2016 Miklos Fazekas. All rights reserved.
+//
+
+#import "XCTestCase+PercySnapshot.h"
+
+#if 0
+@interface XCTestCase()
+- (void) startActivityWithTitle:(NSString*) title block:(void (^)(void)) block;
+@end
+
+@interface XCUIApplication()
+- (void) _waitForQuiescence;
+@end
+
+@implementation XCTestCase (PercySnapshot)
+- (void) percySnapshotWithPath: (NSString*) path
+{
+    [self startActivityWithTitle:[NSString stringWithFormat:@"io.percy/%@", path] block:^{
+        XCUIApplication* app = [[XCUIApplication alloc] init];
+        [app _waitForQuiescence];
+    }];
+}
+@end
+#endif
